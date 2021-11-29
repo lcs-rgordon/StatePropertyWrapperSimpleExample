a04c9ec11c4c7ca1ba21720b731b5544f98ac1dc
Russell Gordon <rgordon@lcs.on.ca>
2021-11-29 11:23:05 -0500

    Initial Commit

---
+++ StatePropertyWrapperSimpleExample/StatePropertyWrapperSimpleExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  StatePropertyWrapperSimpleExampleApp.swift
+//  StatePropertyWrapperSimpleExample
+//
+//  Created by Russell Gordon on 2021-11-29.
+//
+
+import SwiftUI
+
+@main
+struct StatePropertyWrapperSimpleExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
