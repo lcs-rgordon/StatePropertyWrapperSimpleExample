27f0c77d573da308a0ddecb8919944cdeeac8992
Russell Gordon <rgordon@lcs.on.ca>
2021-11-29 11:36:16 -0500

    Got the static user interface laid out.

--- StatePropertyWrapperSimpleExample/ContentView.swift
+++ StatePropertyWrapperSimpleExample/ContentView.swift
@@ -9,13 +9,34 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(alignment: .leading, spacing: 20) {
+            
+            // Input
+            Text("Radius:")
+                .bold()
+            
+            TextField("Radius",
+                      text: .constant(""),
+                      prompt: Text("e.g.: 24.5"))
+            
+            // Output
+            Text("Area:")
+                .bold()
+            
+            Text("24.0 square units")
+            
+            Spacer()
+            
+        }
+        .padding()
+        .navigationTitle("Circle")
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        NavigationView {
+            ContentView()
+        }
     }
 }
