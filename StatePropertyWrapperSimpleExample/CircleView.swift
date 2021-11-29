cbc8b9662da74cdfc1bcb882cb8928fd95707ad3
Russell Gordon <rgordon@lcs.on.ca>
2021-11-29 12:06:47 -0500

    Used a property wrapper, @State, to connect the radius property to a Slider control.

--- StatePropertyWrapperSimpleExample/CircleView.swift
+++ StatePropertyWrapperSimpleExample/CircleView.swift
@@ -10,7 +10,12 @@ import SwiftUI
 struct CircleView: View {
     
     // MARK: Stored properties
-    let radius: Double = 10.0
+    // @State is a "property wrapper"
+    // "radius" is still just a property of the structure
+    // The property wrapper alerts SwiftUI to the fact
+    // that we want changes to this property to show in
+    // the UI.
+    @State var radius: Double = 10.0
     
     // MARK: Computed property
     var area: Double {
@@ -25,10 +30,20 @@ struct CircleView: View {
             Text("Radius:")
                 .bold()
             
-            TextField("Radius",
-                      text: .constant("10.0"),
-                      prompt: Text("e.g.: 24.5"))
-            
+            // The syntax of $ says to use the property, radius, and BIND it to this control
+            // This means when the control changes, the property's value changes
+            Slider(value: $radius,
+                   in: 0.0...100.0,
+                   label: {
+                Text("Radius")
+            },
+                   minimumValueLabel: {
+                Text("0.0")
+            },
+                   maximumValueLabel: {
+                Text("100.0")
+            })
+                        
             // Output
             Text("Area:")
                 .bold()
